b6978ae425a14fcb11b911f99c96f027054f4a4c
Arnel Garrata Jr. <arnelgarratajr@gmail.com>
2026-09-24 16:13:17 +0800

    registered fileprocessing service and added properties for jsonprocessing

---
+++ Data/JsonProcessing.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Text.Json;
+
+namespace GARRATA_09242026.Data;
+
+public class JsonProcessing
+{
+    public class JsonProcessingResult
+{
+    public bool IsValid { get; set; }
+    public string? ErrorMessage { get; set; }
+    public JsonProcessingResponse? Response { get; set; }
+}
+
+public class JsonProcessingResponse
+{
+    public string Message { get; set; } = string.Empty;
+    public string FileName { get; set; } = string.Empty;
+    public int TotalRecords { get; set; }
+    public int MatchingRecords { get; set; }
+    public IReadOnlyList<JsonElement> Records { get; set; } = [];
+}
+}
--- Program.cs
+++ Program.cs
@@ -1,8 +1,11 @@
+using GARRATA_09242026.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+builder.Services.AddScoped<IFileProcessingService, FileProcessingService>();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
